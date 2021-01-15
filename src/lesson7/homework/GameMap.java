2cf50553752e5927ece72dbe28b15df4c4a40d15
Melsen <melsen@yandex.ru>
2021-01-15 18:09:07 +0300

    загрузил результаты выполнения  ДЗ №7

---
+++ src/lesson7/homework/GameMap.java
@@ -0,0 +1,58 @@
+package lesson7.homework;
+import javax.swing.*;
+import java.awt.*;
+
+public class GameMap extends JPanel {
+
+    public static final int GAME_MODE_HVA  = 0;
+    public static final int GAME_MODE_HVH  = 1;
+
+    private int fieldSizeX;
+    private int fieldSizeY;
+    private int winLength;
+    private int[][] field;
+
+    private int cellWidth;
+    private int cellHeight;
+
+
+    GameMap(){
+        setBackground(Color.BLACK);
+    }
+
+
+
+    void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength){
+        this.fieldSizeX = fieldSizeX;
+        this.fieldSizeY = fieldSizeY;
+        this.winLength = winLength;
+        field = new int [fieldSizeX][fieldSizeY];
+        repaint();
+    }
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        render(g);
+    }
+
+    private void render(Graphics g){
+        int width = getWidth();
+        int height = getHeight();
+
+        cellWidth = width / fieldSizeX;
+        cellHeight = height / fieldSizeY;
+        g.setColor(Color.WHITE);
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            int y = i * cellHeight;
+            g.drawLine(0,y,width,y);
+        }
+        for (int i = 0; i < fieldSizeX; i++) {
+            int x = i * cellWidth;
+            g.drawLine(x,0,x,height);
+
+        }
+
+    }
+
+}
---
+++ src/lesson7/homework/Settings.java
@@ -0,0 +1,112 @@
+package lesson7.homework;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Settings extends JFrame {
+    private static final int WIN_WIDTH = 350;
+    private static final int WIN_HEIGHT = 270;
+
+    private static final int MIN_WIN_LENGTH = 3;
+    private static final int MIN_FIELD_LENGTH = 3;
+    private static final int MAX_FIELD_LENGTH = 10;
+
+    private static final String FIELD_SIZE_TEXT_PREFIX = "Размер поля: ";
+    private static final String WINH_LENGTH_TEXT_PREFIX = "Выигрышная серия: ";
+
+    private MainWindow mainWindow;
+    private JRadioButton humVsAi;
+    private JRadioButton humVsHum;
+    private JSlider sliderWinLen;
+    private JSlider sliderFieldSize;
+
+
+
+    Settings(MainWindow mainWindow){
+        this.mainWindow = mainWindow;
+        setSize(WIN_WIDTH,WIN_HEIGHT);
+
+        Rectangle gameWindowBounds = mainWindow.getBounds();
+        int posX = (int)gameWindowBounds.getCenterX() - WIN_WIDTH/2;
+        int posY = (int)gameWindowBounds.getCenterY() - WIN_HEIGHT/2;
+        setLocation(posX,posY);
+        setResizable(false);
+        setTitle("Настройки");
+        setLayout(new GridLayout(10,1));
+        addGameModeSettings();
+        addFieldSizeControl();
+
+        JButton btnStartGame = new JButton("Начать");
+
+        btnStartGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnStartClick();
+            }
+        });
+        add(btnStartGame);
+    }
+
+    private void addGameModeSettings(){
+        add(new JLabel("Выберите режим игры"));
+        humVsAi = new JRadioButton("Human vs. Ai", true);
+        humVsHum = new JRadioButton("Human vs. Human");
+        ButtonGroup gameMode = new ButtonGroup();
+        gameMode.add(humVsAi);
+        gameMode.add(humVsHum);
+        add(humVsAi);
+        add(humVsHum);
+
+    }
+
+    private void addFieldSizeControl(){
+        JLabel lbFieldSize = new JLabel(FIELD_SIZE_TEXT_PREFIX + MIN_FIELD_LENGTH);
+        JLabel lbWinLength = new JLabel(WINH_LENGTH_TEXT_PREFIX + MIN_WIN_LENGTH);
+
+        sliderFieldSize = new JSlider(MIN_FIELD_LENGTH,MAX_FIELD_LENGTH,MIN_FIELD_LENGTH);
+        sliderFieldSize.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int currentValue = sliderFieldSize.getValue();
+                lbFieldSize.setText(FIELD_SIZE_TEXT_PREFIX + currentValue);
+                sliderWinLen.setMaximum(currentValue);
+            }
+        });
+        sliderWinLen = new JSlider(MIN_WIN_LENGTH, MIN_FIELD_LENGTH,MIN_FIELD_LENGTH);
+        sliderWinLen.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                lbWinLength.setText(WINH_LENGTH_TEXT_PREFIX + sliderWinLen.getValue());
+            }
+        });
+        add(new JLabel("Выберите размер поля"));
+        add(lbFieldSize);
+        add(sliderFieldSize);
+        add(new JLabel("Выберите выигрышную серию"));
+        add(lbWinLength);
+        add(sliderWinLen);
+
+    }
+
+    private void btnStartClick(){
+        int gameMode;
+
+        if(humVsAi.isSelected()){
+            gameMode = lesson7.online.GameMap.GAME_MODE_HVA;
+        }else if(humVsHum.isSelected()){
+            gameMode = GameMap.GAME_MODE_HVH;
+        }else {
+            throw new RuntimeException("Неизвестный режим игры");
+        }
+        int fieldSize = sliderFieldSize.getValue();
+        int winLength = sliderWinLen.getValue();
+        mainWindow.startNewGame(gameMode,fieldSize,fieldSize,winLength);
+        setVisible(false);
+
+    }
+
+}
